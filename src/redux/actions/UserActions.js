181576ed2f0effc8ca0a9ce4c523f1cea45a9105
d.terentyev <d.terentyev@freematiq.com>
2018-07-04 17:15:53 +0700

    add vk login

--- src/redux/actions/UserActions.js
+++ src/redux/actions/UserActions.js
@@ -7,28 +7,38 @@ import {
 export function handleLogin() {
 
     return function(dispatch) {
-
         dispatch({
             type: LOGIN_REQUEST
         })
 
-        VK.Auth.login((r) => { // eslint-disable-line no-undef
-            if (r.session) {
-                let username = r.session.user.first_name;
+        VK.Auth.getLoginStatus(function(response) {
+            if (response.session) {
+                let username = response.session.user.first_name;
 
                 dispatch({
                     type: LOGIN_SUCCES,
                     payload: username
-                })
-
+                });
             } else {
-                dispatch({
-                    type: LOGIN_FAIL,
-                    error: true,
-                    payload: new Error('Ошибка авторизации')
-                })
+                VK.Auth.login((r) => { // eslint-disable-line no-undef
+                    if (r.session) {
+                        let username = r.session.user.first_name;
+
+                        dispatch({
+                            type: LOGIN_SUCCES,
+                            payload: username
+                        })
+
+                    } else {
+                        dispatch({
+                            type: LOGIN_FAIL,
+                            error: true,
+                            payload: new Error('Ошибка авторизации')
+                        })
+                    }
+                },4); // запрос прав на доступ к photo
             }
-        },4); // запрос прав на доступ к photo
+        });
     }
 
 }
